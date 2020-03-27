65f6ff4e851040c45af8ccb3b136c73644f0535b
Sungho Park <siminsungho@gmail.com>
2020-03-27 12:39:13 +0900

    Add t32 trace script for borad "nucleof207zg".

---
+++ config/ubinos_nucleof207zg_baremetal_trace.cmake
@@ -0,0 +1,5 @@
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_LOAD                                      "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/nucleof207zg/t32_flash_load_trace.cmm"        PATH)
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_RESET                                     "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/nucleof207zg/t32_flash_reset_trace.cmm"       PATH)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_nucleof207zg_baremetal.cmake)
+
