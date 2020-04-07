38573e733ab66e205c447bc8798453c506043ddd
Sungho Park <siminsungho@gmail.com>
2020-04-07 20:43:18 +0900

    Add ubinos config "ubinos_stm32f769ieval_baremetal_ethernet".

---
+++ config/ubinos_stm32f769ieval_baremetal_ethernet_trace.cmake
@@ -0,0 +1,10 @@
+set_cache(UBINOS__BSP__CLOCK_FREQ_MAIN 108000 STRING)
+set_cache(UBINOS__BSP__BUSYWAITCOUNT_PER_MS 107848 STRING)
+
+set_cache(UBINOS__BSP__STM32_ENABLE_TRACE TRUE BOOL)
+
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_LOAD "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/stm32f769ieval/t32_flash_load_trace.cmm" PATH)
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_RESET "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/stm32f769ieval/t32_flash_reset_trace.cmm" PATH)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_stm32f769ieval_baremetal_ethernet.cmake)
+
