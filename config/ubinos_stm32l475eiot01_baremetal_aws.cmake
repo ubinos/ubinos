7142ee4fbf523319707e8c2a6b568447fbf2debf
Sungho Park <siminsungho@gmail.com>
2020-04-13 19:57:16 +0900

    Add ubinos config "ubinos_stm32l475eiot01_baremetal_aws".

---
+++ config/ubinos_stm32l475eiot01_baremetal_aws.cmake
@@ -0,0 +1,9 @@
+set_cache(UBINOS__UBICLIB__EXCLUDE_HEAP                                           FALSE  BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos/toolchain/arch/arm/arm-none-eabi.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/toolchain.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/bsp/arch/arm/cortexm/stm32l475eiot01/stm32l475eiot01_aws.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/bsp.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/ubiclib.cmake)
+
+
--- make/batch.mk
+++ make/batch.mk
@@ -769,6 +769,18 @@
 	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=ubinos_stm32l475eiot01_baremetal
 	@echo ""
 	@echo ""
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	@echo "Config          : ubinos library"
+	@echo "Target board    : stm32l475eiot01"
+	@echo "OS Kernel       : none"
+	@echo "Link memory map : flash_aws"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=ubinos_stm32l475eiot01_baremetal_aws
+	@echo ""
+	@echo ""
 
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
