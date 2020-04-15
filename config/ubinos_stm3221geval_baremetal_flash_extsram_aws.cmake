2f23b0add0da89c37ebaafd07dcd9db46908075d
Sungho Park <siminsungho@gmail.com>
2020-04-15 14:55:13 +0900

    Add ubinos config "ubinos_stm3221geval_baremetal_flash_extsram_aws", "ubinos_stm3221geval_baremetal_flash_extsram_aws_trace".

---
+++ config/ubinos_stm3221geval_baremetal_flash_extsram_aws.cmake
@@ -0,0 +1,9 @@
+set_cache(UBINOS__UBICLIB__EXCLUDE_HEAP                                           FALSE  BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos/toolchain/arch/arm/arm-none-eabi.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/toolchain.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/bsp/arch/arm/cortexm/stm3221geval/stm3221geval_flash_extsram_aws.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/bsp.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/ubiclib.cmake)
+
+
---
+++ config/ubinos_stm3221geval_baremetal_flash_extsram_aws_trace.cmake
@@ -0,0 +1,11 @@
+set_cache(UBINOS__BSP__LINK_MEMMAP_RAM2_ORIGIN 0x64000000 STRING)
+set_cache(UBINOS__BSP__LINK_MEMMAP_RAM2_LENGTH 0x00080000 STRING)
+
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_LOAD "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/stm3221geval/t32_flash_load_trace.cmm" PATH)
+set_cache(UBINOS__BSP__T32SCRIPT_FILE_RESET "${PROJECT_UBINOS_DIR}/resource/ubinos/bsp/arch/arm/cortexm/stm3221geval/t32_flash_reset_trace.cmm" PATH)
+
+set_cache(UBINOS__BSP__STM32_ENABLE_TRACE TRUE BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_stm3221geval_baremetal_flash_extsram_aws.cmake)
+
+
