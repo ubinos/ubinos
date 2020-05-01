12c86ea4223164e8ac7a43943bf19872f61c2d36
Sungho Park <siminsungho@gmail.com>
2020-05-01 13:21:52 +0900

    Refactoring.

--- source/ubinos/bsp/arch/arm/cortexm/stm32l475/system_stm32l4xx.c
+++ source/ubinos/bsp/arch/arm/cortexm/stm32l475/system_stm32l4xx.c
@@ -266,7 +266,7 @@ void SystemInit(void)
   * @param  None
   * @retval None
   */
-void SystemCoreClockUpdate(void)
+__WEAK void SystemCoreClockUpdate(void)
 {
   uint32_t tmp = 0, msirange = 0, pllvco = 0, pllr = 2, pllsource = 0, pllm = 2;
 
