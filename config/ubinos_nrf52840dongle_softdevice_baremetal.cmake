75ee2a11cb95e2cd6e8e107ab9aef8f9fc9eef24
Sungho Park <siminsungho@gmail.com>
2020-05-14 21:13:06 +0900

    add support for nrf52840dongle board

---
+++ config/ubinos_nrf52840dongle_softdevice_baremetal.cmake
@@ -0,0 +1,6 @@
+set_cache(UBINOS__BSP__BOARD_MODEL "NRF52840DONGLE" STRING)
+set_cache(UBINOS__BSP__USE_DTTY FALSE BOOL)
+
+include(${CMAKE_CURRENT_LIST_DIR}/ubinos_nrf52840dk_softdevice_baremetal.cmake)
+
+
