3abd73c81055a883711fb673c89db3f23ace4bba
Sungho Park <siminsungho@gmail.com>
2020-09-08 10:32:42 +0900

    add ubik_tester_nrf52840dk_bdh ubinos config using bidirectional heap ram memory layout

---
+++ app/ubik_tester_nrf52840dk_bdh.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 Sung Ho Park and CSOS
+# 
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set_cache(UBINOS__BSP__LINKSCRIPT_FILE "${PROJECT_UBINOS_DIR}/source/ubinos/bsp/arch/arm/cortexm/nrf52/xxaa/flash_bdh.ld" PATH)
+
+include(${PROJECT_UBINOS_DIR}/app/ubik_tester_nrf52840dk.cmake)
+
+
