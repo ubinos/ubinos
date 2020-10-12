8981116758448a7db10dcebf695c0da8ab0f1c8f
Sungho Park <siminsungho@gmail.com>
2020-10-12 19:10:06 +0900

    add ubik port for board, STM32429IEVAL - 02

---
+++ app/ubik_tester_stm32429ieval.cmake
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2020 Sung Ho Park and CSOS
+# 
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_stm32429ieval.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos/ubik_test.cmake)
+
+include(${PROJECT_UBINOS_DIR}/app/ubik_tester.cmake)
+
+
