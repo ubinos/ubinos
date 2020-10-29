a4a2ba12e548e750c687b5232e6c18c8d8ca49c5
Sungho Park <siminsungho@gmail.com>
2020-10-29 23:32:06 +0900

    refactoring (change coding conventions to BSD/Allman)

--- app/cli_tester/appmain.c
+++ app/cli_tester/appmain.c
@@ -16,71 +16,80 @@ static void clihelphookfunc();
 static int clihookfunc(char *str, int len, void *arg);
 static int mycmd(char *str, int len, void *arg);
 
-int appmain(int argc, char *argv[]) {
-	int r;
+int appmain(int argc, char *argv[])
+{
+    int r;
 
-	printf("\n\n\n");
-	printf("================================================================================\n");
-	printf("command line interface tester (build time: %s %s)\n", __TIME__, __DATE__);
-	printf("================================================================================\n");
-	printf("\n");
+    printf("\n\n\n");
+    printf("================================================================================\n");
+    printf("command line interface tester (build time: %s %s)\n", __TIME__, __DATE__);
+    printf("================================================================================\n");
+    printf("\n");
 
-	r = cli_sethookfunc(clihookfunc, NULL);
-	if (0 != r) {
-		logme("fail at cli_sethookfunc");
-	}
+    r = cli_sethookfunc(clihookfunc, NULL);
+    if (0 != r)
+    {
+        logme("fail at cli_sethookfunc");
+    }
 
-	r = cli_sethelphookfunc(clihelphookfunc);
-	if (0 != r) {
-		logme("fail at cli_sethelphookfunc");
-	}
+    r = cli_sethelphookfunc(clihelphookfunc);
+    if (0 != r)
+    {
+        logme("fail at cli_sethelphookfunc");
+    }
 
-	r = cli_setprompt("cli_tester> ");
-	if (0 != r) {
-		logme("fail at cli_setprompt");
-	}
+    r = cli_setprompt("cli_tester> ");
+    if (0 != r)
+    {
+        logme("fail at cli_setprompt");
+    }
 
-	r = task_create(NULL, cli_main, NULL, task_getmiddlepriority(), 0, "cli_main");
-	if (0 != r) {
-		logme("fail at task_create");
-	}
+    r = task_create(NULL, cli_main, NULL, task_getmiddlepriority(), 0, "cli_main");
+    if (0 != r)
+    {
+        logme("fail at task_create");
+    }
 
-	ubik_comp_start();
+    ubik_comp_start();
 
-	return 0;
+    return 0;
 }
 
-static int clihookfunc(char *str, int len, void *arg) {
-	int r = -1;
-	char *tmpstr;
-	int tmplen;
-	char *cmd = NULL;
-	int cmdlen;
+static int clihookfunc(char *str, int len, void *arg)
+{
+    int r = -1;
+    char *tmpstr;
+    int tmplen;
+    char *cmd = NULL;
+    int cmdlen;
 
-	tmpstr = str;
-	tmplen = len;
+    tmpstr = str;
+    tmplen = len;
 
-	cmd = "mc";
-	cmdlen = strlen(cmd);
-	if (tmplen == cmdlen && strncmp(tmpstr, cmd, cmdlen) == 0) {
-		tmpstr = &tmpstr[cmdlen];
-		tmplen -= cmdlen;
+    cmd = "mc";
+    cmdlen = strlen(cmd);
+    if (tmplen == cmdlen && strncmp(tmpstr, cmd, cmdlen) == 0)
+    {
+        tmpstr = &tmpstr[cmdlen];
+        tmplen -= cmdlen;
 
-		r = mycmd(tmpstr, tmplen, arg);
-	}
+        r = mycmd(tmpstr, tmplen, arg);
+    }
 
-	return r;
+    return r;
 }
 
-static void clihelphookfunc() {
-	printf("mc                                      : my command\n");
+static void clihelphookfunc()
+{
+    printf("mc                                      : my command\n");
 }
 
-static int mycmd(char *str, int len, void *arg) {
-	printf("\n");
-	printf("done\n");
+static int mycmd(char *str, int len, void *arg)
+{
+    printf("\n");
+    printf("done\n");
 
-	return 0;
+    return 0;
 }
 
 #endif /* (INCLUDE__APP__cli_tester == 1) */
