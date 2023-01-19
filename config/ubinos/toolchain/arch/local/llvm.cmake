a1e94d922f7592f2df97ae7fd1c05eff496cfed4
Sungho Park <siminsungho@gmail.com>
2023-01-19 22:54:45 +0900

    update toolchain/ardch/local/gcc.cmake, toolchain/ardch/local/llvm.cmake.

--- config/ubinos/toolchain/arch/local/llvm.cmake
+++ config/ubinos/toolchain/arch/local/llvm.cmake
@@ -6,4 +6,41 @@
 
 set_cache(PROJECT_TOOLCHAIN_TYPE "LLVM" STRING)
 
+set_cache_default(PROJECT_TOOLCHAIN_C_STD "C99" STRING "C standard version [C99 | GNU11]")
+
+set_cache_default(PROJECT_TOOLCHAIN_CXX_STD "GNU++98" STRING "C++ standard version [GNU++98 | GNU++14]")
+set_cache_default(PROJECT_TOOLCHAIN_CXX_F_NO_RTTI FALSE BOOL "-fno-rtti")
+set_cache_default(PROJECT_TOOLCHAIN_CXX_F_NO_EXCEPTIONS FALSE BOOL "-fno-exceptions")
+set_cache_default(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT FALSE BOOL "-Wno-c++14-compat")
+
 set_cache(PROJECT_TOOLCHAIN_DATA_SECTION_NAME "__data" STRING)
+
+########
+
+if(PROJECT_TOOLCHAIN_C_STD STREQUAL "C99")
+    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=c99")
+elseif(PROJECT_TOOLCHAIN_C_STD STREQUAL "GNU11")
+    set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=gnu11")
+else()
+    message(FATAL_ERROR "Unsupported PROJECT_TOOLCHAIN_C_STD")
+endif()
+
+if(PROJECT_TOOLCHAIN_CXX_STD STREQUAL "GNU++98")
+    set(CMAKE_CXX_FLAGS   "${CMAKE_CXX_FLAGS} -std=gnu++98")
+elseif(PROJECT_TOOLCHAIN_CXX_STD STREQUAL "GNU++14")
+    set(CMAKE_CXX_FLAGS   "${CMAKE_CXX_FLAGS} -std=gnu++14")
+else()
+    message(FATAL_ERROR "Unsupported PROJECT_TOOLCHAIN_CXX_STD")
+endif()
+if(PROJECT_TOOLCHAIN_CXX_F_NO_RTTI)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")
+endif()
+if(PROJECT_TOOLCHAIN_CXX_F_NO_EXCEPTIONS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
+endif()
+if(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-c++14-compat")
+endif()
+
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -x assembler-with-cpp -D__ASSEMBLY__")
+
