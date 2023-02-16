f6e69a663722be22da33e77a484609c3d2c97b6f
Sungho Park <siminsungho@gmail.com>
2023-02-16 13:06:46 +0900

    add ubinos config options for "-fno-unwind-tables", "-fno-threadsafe-statics", "-std=c++11".

--- config/ubinos/toolchain/arch/local/llvm.cmake
+++ config/ubinos/toolchain/arch/local/llvm.cmake
@@ -6,16 +6,25 @@
 
 set_cache(PROJECT_TOOLCHAIN_TYPE "LLVM" STRING)
 
+set_cache_default(PROJECT_TOOLCHAIN_F_NO_UNWIND_TABLES TRUE BOOL "-fno-unwind-tables")
+
 set_cache_default(PROJECT_TOOLCHAIN_C_STD "C99" STRING "C standard version [C99 | GNU11]")
 
 set_cache_default(PROJECT_TOOLCHAIN_CXX_STD "GNU++98" STRING "C++ standard version [GNU++98 | GNU++14]")
 set_cache_default(PROJECT_TOOLCHAIN_CXX_F_NO_RTTI FALSE BOOL "-fno-rtti")
 set_cache_default(PROJECT_TOOLCHAIN_CXX_F_NO_EXCEPTIONS FALSE BOOL "-fno-exceptions")
+set_cache_default(PROJECT_TOOLCHAIN_CXX_F_NO_THREADSAFE_STATICS FALSE BOOL "-fno-threadsafe-statics")
 set_cache_default(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT FALSE BOOL "-Wno-c++14-compat")
 
 set_cache(PROJECT_TOOLCHAIN_DATA_SECTION_NAME "__data" STRING)
 
 ########
+
+if(PROJECT_TOOLCHAIN_F_NO_UNWIND_TABLES)
+    set(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -fno-unwind-tables")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-unwind-tables")
+    set(CMAKE_ASM_FLAGS "${CMAKE_CXX_FLAGS} -fno-unwind-tables")
+endif()
 
 if(PROJECT_TOOLCHAIN_C_STD STREQUAL "C99")
     set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -std=c99")
@@ -29,6 +38,8 @@ if(PROJECT_TOOLCHAIN_CXX_STD STREQUAL "GNU++98")
     set(CMAKE_CXX_FLAGS   "${CMAKE_CXX_FLAGS} -std=gnu++98")
 elseif(PROJECT_TOOLCHAIN_CXX_STD STREQUAL "GNU++14")
     set(CMAKE_CXX_FLAGS   "${CMAKE_CXX_FLAGS} -std=gnu++14")
+elseif(PROJECT_TOOLCHAIN_CXX_STD STREQUAL "C++11")
+    set(CMAKE_CXX_FLAGS   "${CMAKE_CXX_FLAGS} -std=c++11")
 else()
     message(FATAL_ERROR "Unsupported PROJECT_TOOLCHAIN_CXX_STD")
 endif()
@@ -37,6 +48,9 @@ if(PROJECT_TOOLCHAIN_CXX_F_NO_RTTI)
 endif()
 if(PROJECT_TOOLCHAIN_CXX_F_NO_EXCEPTIONS)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions")
+endif()
+if(PROJECT_TOOLCHAIN_CXX_F_NO_THREADSAFE_STATICS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-threadsafe-statics")
 endif()
 if(PROJECT_TOOLCHAIN_CXX_W_NO_CXX14_COMPAT)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-c++14-compat")
