d66aec27203d3f016c16ee026fb1d0a913610c6d
Sungho Park <siminsungho@gmail.com>
2023-09-21 06:07:16 +0900

    Add ubinos config option, UBINOS__BSP__STM32_RCC_TYPE__LSE_MSR for nucleol476rg board.

--- source/ubinos/bsp/arch/arm/cortexm/stm32l475eiot01/bsp.c
+++ source/ubinos/bsp/arch/arm/cortexm/stm32l475eiot01/bsp.c
@@ -12,7 +12,8 @@
 #include "../stm32l4/stm32l4xx_ll_system.h"
 #include "../stm32l4/stm32l4xx_ll_rcc.h"
 
-/* ==============   BOARD SPECIFIC CONFIGURATION CODE BEGIN    ============== */
+#if (UBINOS__BSP__STM32_RCC_TYPE == UBINOS__BSP__STM32_RCC_TYPE__HSE)
+
 /**
   * @brief  System Clock Configuration
   *         The system Clock is configured as follows :
@@ -68,6 +69,111 @@ __WEAK void SystemClock_Config(void)
   //LL_SetSystemCoreClock(80000000);
   SystemCoreClock = (UBINOS__BSP__CLOCK_FREQ_MAIN * 1000);
 }
+
+#elif (UBINOS__BSP__STM32_RCC_TYPE == UBINOS__BSP__STM32_RCC_TYPE__LSE_MSR)
+
+#if (STM32CUBEL4__USE_HAL_DRIVER == 1)
+#else
+  #error "To use UBINOS__BSP__STM32_RCC_TYPE__LSE_MSR, set STM32CUBEL4__USE_HAL_DRIVER to 1"
+#endif /* (STM32CUBEL4__USE_HAL_DRIVER == 1) */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *
+  *         If define USB_USE_LSE_MSI_CLOCK enabled:
+  *            System Clock source            = PLL (MSI)
+  *            SYSCLK(Hz)                     = 80000000
+  *            HCLK(Hz)                       = 80000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            APB2 Prescaler                 = 2
+  *            MSI Frequency(Hz)              = 4800000
+  *            LSE Frequency(Hz)              = 32768
+  *            PLL_M                          = 6
+  *            PLL_N                          = 40
+  *            PLL_P                          = 7
+  *            PLL_Q                          = 4
+  *            PLL_R                          = 4
+  *            Flash Latency(WS)              = 4
+  * 
+  * @param  None
+  * @retval None
+  */
+__WEAK void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+
+  /* Enable the LSE Oscilator */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_OFF;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  
+  /* Enable the CSS interrupt in case LSE signal is corrupted or not present */
+  HAL_RCCEx_DisableLSECSS();
+  
+  /* Enable MSI Oscillator and activate PLL with MSI as source */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState            = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange       = RCC_MSIRANGE_11;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM            = 6;
+  RCC_OscInitStruct.PLL.PLLN            = 40;
+  RCC_OscInitStruct.PLL.PLLP            = 7;
+  RCC_OscInitStruct.PLL.PLLQ            = 4;
+  RCC_OscInitStruct.PLL.PLLR            = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Enable MSI Auto-calibration through LSE */
+  HAL_RCCEx_EnableMSIPLLMode();
+
+  /* Select MSI output as USB clock source */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_MSI;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+  clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  
+  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
+  //LL_SetSystemCoreClock(80000000);
+  SystemCoreClock = (UBINOS__BSP__CLOCK_FREQ_MAIN * 1000);
+}
+
+#else
+
+  #error "Unsupported UBINOS__BSP__STM32_RCC_TYPE"
+
+#endif /* (UBINOS__BSP__STM32_RCC_TYPE == UBINOS__BSP__STM32_RCC_TYPE__...) */
 
 __WEAK void MPU_Config(void) {
 }
