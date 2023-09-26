8876f90bccabc7b5285faf234c5ab8402a26b1da
Sungho Park <siminsungho@gmail.com>
2023-09-27 05:44:57 +0900

    add ubinos config, ubinos_thingy52_baremetal, ubinos_thingy52.

---
+++ config/ubinos_thingy52.cmake
@@ -0,0 +1,13 @@
+#
+#
+# Copyright (c) 2019 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "ubinos", "build_type": "cmake_ubinos"}
+
+set_cache(UBINOS__BSP__BOARD_VARIATION_NAME "THINGY52" STRING)
+
+include(${CMAKE_CURRENT_LIST_DIR}/ubinos_nrf52dk.cmake)
+
