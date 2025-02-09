bbbeb4a5ccfd7f7add072e1376b5ea5055396d55
Sungho Park <siminsungho@gmail.com>
2025-02-10 04:23:06 +0900

    Add make_sphinx.

---
+++ make_sphinx/makefile.mk
@@ -0,0 +1,87 @@
+###############################################################################
+
+LIBRARY_DIR ?= ../..
+
+###############################################################################
+
+include $(LIBRARY_DIR)/ubinos/make/common.mk
+-include $(LIBRARY_DIR)/ubinos/make/custom.mk
+
+###############################################################################
+
+include $(_CONFIG_DIR)/$(_CONFIG_NAME).mk
+
+###############################################################################
+
+config:
+	$(call begin_message)
+	$(_PRECMD) && $(call func_make_dir,"$(_OUTPUT_DIR)")
+	$(call end_message)
+
+menuconfig:
+	$(call begin_message)
+	$(call end_message)
+
+xconfig:
+	$(call begin_message)
+	$(call end_message)
+
+build: doc
+	$(call begin_message)
+	$(call end_message)
+
+load:
+	$(call begin_message)
+	$(call end_message)
+
+reset:
+	$(call begin_message)
+	$(call end_message)
+
+run:
+	$(call begin_message)
+	$(call end_message)
+
+xrun: xopendoc
+	$(call begin_message)
+	$(call end_message)
+
+doc:
+	$(call begin_message)
+ifneq ("$(BUILD_HTML)", "0")
+	$(_PRECMD) && sphinx-build -M html     "$(_CONFIG_DIR)/$(APP__NAME)/doc" "$(_OUTPUT_DIR)"
+endif
+ifneq ("$(BUILD_LATEXPDF)", "0")
+	$(_PRECMD) && sphinx-build -M latexpdf "$(_CONFIG_DIR)/$(APP__NAME)/doc" "$(_OUTPUT_DIR)"
+endif
+	$(call end_message)
+
+xopendoc:
+	$(call begin_message)
+ifneq ("$(BUILD_HTML)", "0")
+	$(shell python "$(_TOOLBOX)" get_open_command_for_cmake) "$(_OUTPUT_DIR)/html/index.html"
+endif
+ifneq ("$(BUILD_LATEXPDF)", "0")
+	$(shell python "$(_TOOLBOX)" get_open_command_for_cmake) "$(_OUTPUT_DIR)/latex/$(LATEX_FILE_NAME).pdf"
+endif
+	$(call end_message)
+
+cleandoc:
+	$(call begin_message)
+	$(_PRECMD) && sphinx-build -M clean    "$(_CONFIG_DIR)/$(APP__NAME)/doc" "$(_OUTPUT_DIR)"
+	$(call end_message)
+
+clean: cleandoc
+	$(call begin_message)
+	$(call end_message)
+
+cleand:
+	$(call begin_message)
+	$(_PRECMD) && $(call func_remove_dir,"$(_OUTPUT_DIR)")
+	$(call end_message)
+
+%: common-% ;
+
+###############################################################################
+
+
