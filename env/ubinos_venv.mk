cb7f37f34b7aa409fa52ea8a84e11f9e6035592f
Sungho Park <siminsungho@gmail.com>
2025-02-11 00:34:35 +0900

    Refactoring.

--- env/ubinos_venv.mk
+++ env/ubinos_venv.mk
@@ -1,4 +1,4 @@
-# ubinos_config_info {"name_base": "ubinos_venv", "build_type": "make", "make_dir": "library/ubinos/make_python", "include_sub_dir": true}
+# ubinos_config_info {"name_base": "ubinos_venv", "build_type": "make", "make_dir": "library/ubinos/make_venv", "include_sub_dir": true}
 
 APP__NAME = ubinos_venv
 
