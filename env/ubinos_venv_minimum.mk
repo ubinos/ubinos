d56bd02cbcc7599a4f17bcbb69309aa5255f1fa4
Sungho Park <siminsungho@gmail.com>
2025-07-19 18:55:47 +0900

    ubinos_venv_minimum: Add

---
+++ env/ubinos_venv_minimum.mk
@@ -0,0 +1,7 @@
+# ubinos_config_info {"name_base": "ubinos_venv_minimum", "build_type": "make", "make_dir": "library/ubinos/make_venv", "include_sub_dir": true}
+
+APP__NAME = ubinos_venv_minimum
+
+# PYTHON_INTERPRETER = python3.13
+# PYTHON_VENV_INTERPRETER = python3.11
+
