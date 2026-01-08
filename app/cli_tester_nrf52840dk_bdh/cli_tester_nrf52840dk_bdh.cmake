81b53458972f8a3491594b5e65c8db704d883610
Sungho Park <siminsungho@gmail.com>
2026-01-08 18:56:40 +0900

    Apply ubinos new build structure - 02.

---
+++ app/cli_tester_nrf52840dk_bdh/cli_tester_nrf52840dk_bdh.cmake
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2019 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "cli_tester", "build_type": "cmake_ubinos", "app": true}
+
+set_cache(UBINOS__BSP__LINKSCRIPT_FILE "${PROJECT_UBINOS_DIR}/source/ubinos/bsp/arch/arm/cortexm/nrf52/xxaa/flash_bdh.ld" PATH)
+
+set_cache(UBINOS__UBICLIB__HEAP_DEFAULT_DIR 1 STRING)
+
+include(${CMAKE_CURRENT_LIST_DIR}/../cli_tester_nrf52840dk/cli_tester_nrf52840dk.cmake)
+
